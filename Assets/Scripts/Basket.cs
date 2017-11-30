ca6e253ced7933c3cefb2c47c25094fde756e434
draced <danieldarnold@gmail.com>
2017-11-30 12:00:29 -0600

    Initial Upload

---
+++ Assets/Scripts/Basket.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; 
+
+public class Basket : MonoBehaviour
+{
+    public AudioSource basket; //reference to the basket sound
+    public Text scoreText; //score text 
+    private int score = 0; //used to incremement our score 
+
+    void OnTriggerEnter(Collider col) //if the ball hits the collider inside the net
+    {
+        if (col.gameObject.tag == "Basket")
+        {
+            ScoreUpdate(); //update the score 
+        }
+        basket = GetComponent<AudioSource>();
+
+    }
+
+    private void ScoreUpdate() //update the score 
+    {
+        score++;
+        scoreText.text = score.ToString();
+        if (!basket.isPlaying) 
+        {
+            basket.Play();
+        }
+        else {
+            //not in the basket 
+            basket.Stop();
+        }
+    }
+}
